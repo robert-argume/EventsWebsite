262f007db8f1e35d0f1223f3746c59a561b3d79a
Robert <robert.a.argume@gmail.com>
2018-12-09 19:10:15 -0500

    Creates Event Entity using Doctrine Entity Manager. Creates play.php  script that boots Symfony server and run test scripts.

---
+++ play.php
@@ -0,0 +1,41 @@
+<?php
+
+use Symfony\Component\Debug\Debug;
+use Symfony\Component\HttpFoundation\Request;
+use EventBundle\Entity\Event;
+
+require __DIR__.'/app/autoload.php';
+Debug::enable();
+
+$kernel = new AppKernel('dev', true);
+$kernel->loadClassCache();
+$request = Request::createFromGlobals();
+$kernel->boot();
+
+// Get container and set request
+$container = $kernel->getContainer();
+$container->enterScope('request');
+$container->set('request', $request);
+
+// Get templating service and render a template, passing parameter accordingly
+// $templating = $container->get('templating');
+// echo $templating->render(
+//     'EventBundle:Default:index.html.twig',
+//     array( 'name' => 'Vader')
+// );
+
+// Create an event 
+$event = new Event();
+$event->setName('John\' suprise birthday party');
+$event->setLocation('Centennial College');
+$birthdayDateTime = new DateTime('2018-12-23 23:00:00');
+$birthdayDateTime->format('Y-m-d H:i:s');
+$event->setTime($birthdayDateTime);
+$event->setDetails("John HATES surprises!");
+
+// Save it using Doctrine Entity Manager
+$em = $container->get('doctrine')->getManager();
+$em->persist($event);
+$em->flush();
+
+
